904fb08670946f5c28e22066c4b8009056177259
Vikram Saini <vikramjit.personal@gmail.com>
2023-11-09 17:09:41 +0530

    using useReducer hook

--- src/components/DateCounter.jsx
+++ src/components/DateCounter.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
 function DateCounter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
+  const [question, dispatch] = useReducer(reducer, 0);
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
