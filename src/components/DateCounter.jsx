a11e374cc24c75a5eedac805c53d7cb4f5ca0db5
Vikram Saini <vikramjit.personal@gmail.com>
2023-11-14 19:37:57 +0530

    using useReducer hook

--- src/components/DateCounter.jsx
+++ src/components/DateCounter.jsx
@@ -13,10 +13,6 @@ function DateCounter() {
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
-  function dispatch() {
-    console.log("inside dispatch");
-  }
-
   function dec() {
     // setCount((count) => count - 1);
     setCount((count) => count - step);
